635d56bf3b0f3d33e9e0fb2c43bfc9b9e68e359d
gustavosantossss <156330548+gustavosantossss@users.noreply.github.com>
2024-01-18 17:38:54 -0300

    Add files via upload

---
+++ area-simples-python.py
@@ -0,0 +1,11 @@
+a, b, c = input().slipt()
+
+a = float(a)
+b = float(b)
+c = float(c)
+
+print(f'TRIANGULO: {(a * c) / 2:.3f}')
+print(f'CIRCULO: {3.14159 * C**2:.3f}')
+print(f'TRAPEZIO: {((a + b)) / 2:.3f}')
+print(f'QUADRADO: {b**2:.3f}')
+print(f'RETANGULO: {a * b:.3f}')
